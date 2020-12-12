5b173d3dbe63e3161e178c7ed777a246e01dbe57
Laura North <laura.north@helsinki.fi>
2020-12-12 22:22:15 +0200

    Tehtävät 3-4, tehty refaktorointi

--- KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSTekoaly.java
+++ KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSTekoaly.java
@@ -1,43 +1,15 @@
 package ohtu.kivipaperisakset;
 
 import java.util.Scanner;
+import ohtu.kivipaperisakset.Tekoaly;
 
-public class KPSTekoaly {
+public class KPSTekoaly extends KiviPaperiSakset {
 
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public void pelaa() {
-        Tuomari tuomari = new Tuomari();
-        Tekoaly tekoaly = new Tekoaly();
-
-        System.out.println("Ensimmäisen pelaajan siirto: ");
-        String ekanSiirto = scanner.nextLine();
-        String tokanSiirto;
-
-        tokanSiirto = tekoaly.annaSiirto();
+    @Override
+    protected String toisenSiirto() {
+        Tekoaly tekoaly = new Tekoaly();        
+        String tokanSiirto = tekoaly.annaSiirto();
         System.out.println("Tietokone valitsi: " + tokanSiirto);
-
-
-        while (onkoOkSiirto(ekanSiirto) && onkoOkSiirto(tokanSiirto)) {
-            tuomari.kirjaaSiirto(ekanSiirto, tokanSiirto);
-            System.out.println(tuomari);
-            System.out.println();
-
-            System.out.println("Ensimmäisen pelaajan siirto: ");
-            ekanSiirto = scanner.nextLine();
-
-            tokanSiirto = tekoaly.annaSiirto();
-            System.out.println("Tietokone valitsi: " + tokanSiirto);
-            tekoaly.asetaSiirto(ekanSiirto);
-
-        }
-
-        System.out.println();
-        System.out.println("Kiitos!");
-        System.out.println(tuomari);
-    }
-
-    private static boolean onkoOkSiirto(String siirto) {
-        return "k".equals(siirto) || "p".equals(siirto) || "s".equals(siirto);
-    }
+        return tokanSiirto;
+    }      
 }
